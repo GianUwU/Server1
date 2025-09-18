105194ad5751346506b40508d2b8633937b602e4
GianUwU <76407261+GianUwU@users.noreply.github.com>
2025-09-18 14:50:53 +0200

    V7

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Hello from App.vue 🎉</h1>
+    <MyButton />
+  </div>
+</template>
+
+<script>
+import MyButton from './components/MyButton.vue'
+
+export default {
+  name: 'App',
+  components: { MyButton }
+}
+</script>
+
+<style>
+h1 {
+  color: dodgerblue;
+}
+</style>
